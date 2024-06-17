b40bf4d57b7bb5fb21aabeeb72aaa91ac91baf13
Welldar <eldarzheryakov@gmail.com>
2024-06-17 14:51:12 +0500

    feat: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './components/App/App'
+import { Provider } from 'react-redux'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { User } from './components/User/User'
+import { store } from './components/App/store'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  {
+    path: 'users/:id',
+    element: <User />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
