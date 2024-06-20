ac7033b0fc931c264613e5b8f06ea236b2e347f8
Welldar <eldarzheryakov@gmail.com>
2024-06-20 22:54:24 +0500

    feat: add styles for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,30 @@
 import { useDispatch } from 'react-redux'
 import { signOut } from '../auth/signUpSlice'
+import { Button } from '../Button/Button'
 
-export function Header({ children }: { children: React.ReactNode }) {
+export function Header({
+  children,
+  leftColumn,
+}: {
+  children: React.ReactNode
+  leftColumn?: React.ReactNode
+}) {
   const dispatch = useDispatch()
 
   return (
-    <header>
-      <button
-        onClick={() => {
-          dispatch(signOut())
-        }}
-      >
-        Выход
-      </button>
-      {children}
+    <header className="mb-12 bg-purple-800 pt-8 text-white">
+      <div className="mx-auto grid max-w-screen-xl grid-cols-[minmax(90px,auto)_1fr_minmax(90px,auto)] items-start gap-2">
+        <div>{leftColumn}</div>
+        <div>{children}</div>
+        <Button
+          className="border-zinc-50"
+          clickHandler={() => {
+            dispatch(signOut())
+          }}
+        >
+          <span className="text-zinc-50">Выход</span>
+        </Button>
+      </div>
     </header>
   )
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,9 +5,9 @@ export function Main() {
   return (
     <>
       <Header>
-        <div className="">
-          <h1>Наша команда</h1>
-          <p>
+        <div className="mx-auto mb-16 mt-8 flow-root max-w-4xl text-center">
+          <h1 className="mb-4 text-6xl">Наша команда</h1>
+          <p className="text-xl text-zinc-50">
             Это опытные специалисты, хорошо разбирающиеся во всех задачах,
             которые ложатся на их плечи, и умеющие находить выход из любых, даже
             самых сложных ситуаций.
