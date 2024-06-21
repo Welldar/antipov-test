300f4beff9c80cd18dc9cba9b009ffd0fbd3ad2c
Welldar <eldarzheryakov@gmail.com>
2024-06-21 13:16:34 +0500

    feat: center form

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -22,58 +22,60 @@ export function SignUp() {
 
   return (
     <FormProvider {...methods}>
-      <form
-        className="mx-auto my-4 flex max-w-[500px] flex-col gap-4 rounded-2xl p-4 shadow-[0_4px_20px_rgba(0,0,0,0.08)]"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h2 className="text-xl">Регистрация</h2>
-        <Input name="name" labelTxt="Имя" placeholder="Артур" />
-        <Input
-          name="email"
-          labelTxt="Электронная почта"
-          placeholder="example@mail.ru"
-          rules={{
-            required: 'Необходимо указать email',
-            pattern: {
-              value: /^\S+@\S+$/i,
-              message: 'Некорректный email',
-            },
-          }}
-        />
-        <PasswordInput
-          name="password"
-          labelTxt="Пароль"
-          placeholder="******"
-          type="password"
-          rules={{
-            required: 'Необходимо указать пароль',
-            minLength: { value: 6, message: 'Минимальная длина 6 символов' },
-            onChange: () => {
-              getFieldState('confirmPassword').isDirty &&
-                trigger('confirmPassword')
-            },
-          }}
-        />
-        <PasswordInput
-          name="confirmPassword"
-          labelTxt="Подтвердите пароль"
-          placeholder="******"
-          type="password"
-          rules={{
-            validate: (value) => {
-              if (getValues('password') != value) {
-                return 'Пароли не совпадают'
-              }
-            },
-          }}
-        />
-        <button
-          className="mt-2 rounded-lg bg-violet-900 px-3 py-2 text-white"
-          type="submit"
+      <div className="grid h-screen grid-cols-1 items-center py-2">
+        <form
+          className="mx-auto flex w-full max-w-[500px] flex-col gap-4 rounded-2xl p-4 shadow-[0_4px_20px_rgba(0,0,0,0.08)]"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Зарегистрироваться
-        </button>
-      </form>
+          <h2 className="text-xl">Регистрация</h2>
+          <Input name="name" labelTxt="Имя" placeholder="Артур" />
+          <Input
+            name="email"
+            labelTxt="Электронная почта"
+            placeholder="example@mail.ru"
+            rules={{
+              required: 'Необходимо указать email',
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: 'Некорректный email',
+              },
+            }}
+          />
+          <PasswordInput
+            name="password"
+            labelTxt="Пароль"
+            placeholder="******"
+            type="password"
+            rules={{
+              required: 'Необходимо указать пароль',
+              minLength: { value: 6, message: 'Минимальная длина 6 символов' },
+              onChange: () => {
+                getFieldState('confirmPassword').isDirty &&
+                  trigger('confirmPassword')
+              },
+            }}
+          />
+          <PasswordInput
+            name="confirmPassword"
+            labelTxt="Подтвердите пароль"
+            placeholder="******"
+            type="password"
+            rules={{
+              validate: (value) => {
+                if (getValues('password') != value) {
+                  return 'Пароли не совпадают'
+                }
+              },
+            }}
+          />
+          <button
+            className="mt-2 rounded-lg bg-purple-800 px-3 py-2 text-white"
+            type="submit"
+          >
+            Зарегистрироваться
+          </button>
+        </form>
+      </div>
     </FormProvider>
   )
 }
